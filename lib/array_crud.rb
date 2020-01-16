c1e7a6a1a942d3b48d259b5df60e057e0113d8d2
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 17:09:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
+array = ["wow", "I", "am", "really", "learning"]
 array<<"arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 
 end
 
